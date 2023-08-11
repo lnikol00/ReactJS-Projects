b9caaa370188b35afc6105388a130fc326d8b143
Koce1 <122328343+lnikol00@users.noreply.github.com>
2023-08-11 12:48:18 +0200

    first commit

---
+++ project5/src/components/context/AnimatedPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AnimatedPage() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AnimatedPage
---
+++ project5/src/components/context/AnimatedRoutes.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AnimatedRoutes() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AnimatedRoutes
